a5bd07414ad042b5552397c121d00a54a0581e4c
Jan Eike Suchard <jan@suchard.cloud>
2024-02-22 14:44:40 +0100

    remvoing unused variable

--- globals/variables.go
+++ globals/variables.go
@@ -8,7 +8,7 @@ import (
 // This file contains globally shared variables (e.g., service name, sql queries)
 
 // ServiceName contains the global identifier for the service
-const ServiceName = "template-service"
+const ServiceName = "smartmeter-data"
 
 // SqlQueries contains the prepared sql queries from the resources folder
 var SqlQueries *dotsql.DotSql
@@ -21,6 +21,3 @@ var AuthorizationConfiguration wisdomType.AuthorizationConfiguration
 // they were set to. However, this variable only contains the configured environment
 // variables
 var Environment map[string]string = make(map[string]string)
-
-// Errors contains all errors that have been predefined in the "errors.json" file.
-var Errors map[string]wisdomType.WISdoMError = make(map[string]wisdomType.WISdoMError)
