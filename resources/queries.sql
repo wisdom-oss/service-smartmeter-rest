b2b9bfea6f44cec30e797edf330ff6d4579b0e5e
Jan Eike Suchard <jan@suchard.cloud>
2024-02-27 16:09:20 +0100

    fixing sql queries

--- resources/queries.sql
+++ resources/queries.sql
@@ -1,5 +1,32 @@
--- name: template-query
-CREATE TABLE template (
-    id serial,
-    name text
-);
+-- name: timeseries-overview
+SELECT smart_meter,
+    MAX(time) AS last_entry,
+    MIN(time) AS first_entry
+FROM time_series.smart_meter_data
+GROUP BY smart_meter;
+
+-- name: timeseries-exists
+SELECT EXISTS(SELECT smart_meter_data.smart_meter FROM time_series.smart_meter_data WHERE smart_meter = $1);
+
+-- name: timeseries
+SELECT time, flow_rate
+FROM time_series.smart_meter_data
+WHERE smart_meter = $1;
+
+-- name: timeseries-daterange
+SELECT time, flow_rate
+FROM time_series.smart_meter_data
+WHERE smart_meter = $1
+  AND time BETWEEN $2 AND $3;
+
+-- name: timeseries-daterange-from
+SELECT time, flow_rate
+FROM time_series.smart_meter_data
+WHERE smart_meter = $1
+  AND time > $2
+
+-- name: timeseries-daterange-until
+SELECT time, flow_rate
+FROM time_series.smart_meter_data
+WHERE smart_meter = $1
+  AND time < $2
