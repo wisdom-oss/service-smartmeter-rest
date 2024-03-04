2bdfee4a20fd4311650f46061bb6554270d2bdff
Jan Eike Suchard <jan@suchard.cloud>
2024-03-04 10:19:34 +0100

    adding tags for cbor output

--- types/timeseries-datapoint.go
+++ types/timeseries-datapoint.go
@@ -3,6 +3,6 @@ package types
 import "github.com/jackc/pgx/v5/pgtype"
 
 type DataPoint struct {
-	Timestamp pgtype.Timestamptz `json:"timestamp" db:"time"`
-	Value     pgtype.Float8      `json:"value" db:"flow_rate"`
+	Timestamp pgtype.Timestamptz `json:"timestamp" db:"time" cbor:"1"`
+	Value     pgtype.Float8      `json:"value" db:"flow_rate" cbor:"2"`
 }
