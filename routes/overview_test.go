d90554ff62d7e0c03495fad1ec34f285cea6f90a
Jan Eike Suchard <jan@suchard.cloud>
2024-03-04 10:21:16 +0100

    updated loading of api contract

--- routes/overview_test.go
+++ routes/overview_test.go
@@ -12,22 +12,22 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
 	"github.com/qustavo/dotsql"
-	wisdomType "github.com/wisdom-oss/commonTypes"
+	wisdomType "github.com/wisdom-oss/commonTypes/v2"
 	middleware "github.com/wisdom-oss/microservice-middlewares/v4"
 	validator "openapi.tanna.dev/go/validator/openapi3"
 
 	"github.com/wisdom-oss/service-smartmeter-rest/globals"
 )
 
+var contract *openapi3.T
+
+const responseFormatString = "Response Code: %d\nResponse Status:%s\nContent-Length: %s\nContent-Type: %s\n\n%s"
+
 func TestMain(m *testing.M) {
 	// load the variables found in the .env file into the process environment
 	godotenv.Load()
-	location, locationChanged := os.LookupEnv("ENV_CONFIG_LOCATION")
-	if !locationChanged {
-		location = "./environment.json"
-	}
 	var c wisdomType.EnvironmentConfiguration
-	c.PopulateFromFilePath(location)
+	c.PopulateFromFilePath("./resources/environment.json")
 	globals.Environment, _ = c.ParseEnvironment()
 	address := fmt.Sprintf("postgres://%s:%s@%s:%s/wisdom",
 		globals.Environment["PG_USER"], globals.Environment["PG_PASS"],
@@ -35,15 +35,17 @@ func TestMain(m *testing.M) {
 
 	config, _ := pgxpool.ParseConfig(address)
 	globals.Db, _ = pgxpool.NewWithConfig(context.Background(), config)
-	globals.SqlQueries, _ = dotsql.LoadFromFile(globals.Environment["QUERY_FILE_LOCATION"])
+	globals.SqlQueries, _ = dotsql.LoadFromFile("./resources/queries.sql")
+
+	var err error
+	contract, err = openapi3.NewLoader().LoadFromFile("./openapi.yaml")
+	if err != nil {
+		panic(err)
+	}
 	os.Exit(m.Run())
 }
 
 func TestOverview_JSON(t *testing.T) {
-	contract, err := openapi3.NewLoader().LoadFromFile("./openapi.yaml")
-	if err != nil {
-		t.Errorf("unable to load openapi contract: %s", err.Error())
-	}
 	router := chi.NewRouter()
 	router.Use(middleware.ErrorHandler)
 	router.Get("/", Overview)
