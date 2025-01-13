31f1c115f567e74db1a2beca175d18b2aeae616b
Jan Eike Suchard <jan@suchard.cloud>
2025-01-13 12:51:02 +0100

    chore: dropping smartmeter id from datapoint

--- types/datapoint.go
+++ types/datapoint.go
@@ -1,9 +1,6 @@
 package types
 
-import "github.com/jackc/pgx/v5/pgtype"
-
 type DataPoint struct {
-	SmartMeter int                `db:"smart_meter" json:"smartMeter"`
-	Timestamp  pgtype.Timestamptz `db:"time"        json:"timestamp"`
-	FlowRate   float64            `db:"flow_rate"   json:"flowRate"`
+	Timestamp Timestamp `csv:"timestamp" db:"time"      json:"timestamp"`
+	FlowRate  float64   `csv:"flow_rate" db:"flow_rate" json:"flowRate"`
 }
