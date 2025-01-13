334d5fbeda14abd1d306e65a96406b091430f182
Jan Eike Suchard <jan@suchard.cloud>
2025-01-13 12:20:26 +0100

    chore: adding returning to insert and adding existence query to smart meters

--- resources/meter-queries.sql
+++ resources/meter-queries.sql
@@ -16,9 +16,21 @@ WHERE
 INSERT INTO
     geodata.smartmeters ("name", "key", geometry, additional_properties)
 VALUES
-    ($1, $2, $3, $4);
+    ($1, $2, ST_Point ($3, $4, 4326), $5)
+RETURNING
+*;
 
 -- name: delete-meter
 DELETE FROM geodata.smartmeters
 WHERE
     id=$1;
+
+-- name: meter-exists
+SELECT
+    EXISTS (
+SELECT *
+        FROM
+            geodata.smartmeters
+        WHERE
+            id=$1
+    );
