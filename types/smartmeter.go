0d69c5c36e14d33e86778387e1f7e124bf57b22e
Jan Eike Suchard <jan@suchard.cloud>
2025-01-13 12:50:35 +0100

    chore: reworking smartmeter serialization

--- types/smartmeter.go
+++ types/smartmeter.go
@@ -7,26 +7,31 @@ import (
 	"github.com/twpayne/go-geom/encoding/geojson"
 )
 
+const coordinatePrecision = 15
+
 var geoJsonMarshalOptions = []geojson.EncodeGeometryOption{
 	geojson.EncodeGeometryWithBBox(),
-	geojson.EncodeGeometryWithMaxDecimalDigits(15), //nolint:mnd
+	geojson.EncodeGeometryWithMaxDecimalDigits(coordinatePrecision),
 }
 
-// SmartMeter represents real world smart meter.
-type SmartMeter struct {
-	ID                   int                    `db:"id"                    json:"id"`
-	Geometry             geom.T                 `db:"geometry"`
-	Name                 *string                `db:"name"                  json:"name"`
-	Key                  string                 `db:"key"                   json:"key"`
-	AdditionalProperties map[string]interface{} `db:"additional_properties" json:"additionalProperties"`
+// Smartmeter represents real world smart meter.
+type Smartmeter struct {
+	ID                   int            `db:"id"`
+	Geometry             geom.T         `db:"geometry"`
+	Name                 *string        `db:"name"`
+	Key                  string         `db:"key"`
+	AdditionalProperties map[string]any `db:"additional_properties"`
 }
 
 // MarshalJSON implements the [json.Marshaler] interface and is used to convert
 // the database depiction of the geometry to GeoJSON.
-func (s SmartMeter) MarshalJSON() ([]byte, error) {
+func (s Smartmeter) MarshalJSON() ([]byte, error) {
 	var out struct {
-		SmartMeter
-		Geometry json.RawMessage `json:"geometry"`
+		ID                   int             `json:"id"`
+		Geometry             json.RawMessage `json:"geometry"`
+		Name                 *string         `json:"name"`
+		Key                  string          `json:"key"`
+		AdditionalProperties map[string]any  `json:"additionalProperties"`
 	}
 	encodedGeometry, err := geojson.Marshal(s.Geometry, geoJsonMarshalOptions...)
 	if err != nil {
