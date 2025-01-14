497bd0ed448e1a2c5407cd011b1b28a2572b645a
Jan Eike Suchard <jan@suchard.cloud>
2025-01-14 11:28:33 +0100

    fix: only using time and flow_rate from data table

--- resources/data-queries.sql
+++ resources/data-queries.sql
@@ -1,6 +1,7 @@
 -- name: get-data
 SELECT
-    *
+    "time",
+    flow_rate
 FROM
     timeseries.smartmeter_data
 WHERE
@@ -8,28 +9,34 @@ WHERE
 
 -- name: get-data-from
 SELECT
-    *
+    "time",
+    flow_rate
 FROM
     timeseries.smartmeter_data
 WHERE
     smart_meter=$1
-    AND "time">$2;
+    AND "time">=$2
+ORDER BY "time";
 
 -- name: get-data-until
 SELECT
-    *
+    "time",
+    flow_rate
 FROM
     timeseries.smartmeter_data
 WHERE
     smart_meter=$1
-    AND "time"<$2;
+    AND "time"<=$2
+ORDER BY "time";
 
 -- name: get-data-in-range
 SELECT
-    *
+    "time",
+    flow_rate
 FROM
     timeseries.smartmeter_data
 WHERE
     smart_meter=$1
-    AND "time">$2
-    AND "time"<$3;
+    AND "time">=$2
+    AND "time"<=$3
+ORDER BY "time";
