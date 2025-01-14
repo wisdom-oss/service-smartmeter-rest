ee6ff2b5eb97c4c1f98d68062b75c711357bf8b2
Jan Eike Suchard <jan@suchard.cloud>
2025-01-14 11:25:54 +0100

    chore(docs): adding information about autoload

--- internal/configuration_debug.go
+++ internal/configuration_debug.go
@@ -2,4 +2,9 @@
 
 package internal
 
+// This file ensures that environment variables from .env files are
+// automatically loaded on application start.
+// As this file is exluded from the release build, this behaviour will only
+// be disabled in Docker Images and builds using the release tag
+
 import _ "github.com/joho/godotenv/autoload"
