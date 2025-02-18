8031babd1ca1fbb493ea068a67014a2251a57a7f
Jan Eike Suchard <jan@suchard.cloud>
2025-02-18 09:41:13 +0100

    chore(db): adding queries for updating smartmeters

--- resources/meter-queries.sql
+++ resources/meter-queries.sql
@@ -10,7 +10,7 @@ SELECT
 FROM
     geodata.smartmeters
 WHERE
-    id=$1;
+    key=$1;
 
 -- name: insert-meter
 INSERT INTO
@@ -23,14 +23,36 @@ RETURNING
 -- name: delete-meter
 DELETE FROM geodata.smartmeters
 WHERE
-    id=$1;
+    key=$1;
 
 -- name: meter-exists
 SELECT
     EXISTS (
-SELECT *
+        SELECT
+            *
         FROM
             geodata.smartmeters
         WHERE
-            id=$1
+            key=$1
     );
+
+-- name: update-meter-name
+UPDATE geodata.smartmeters
+SET
+    name=$1
+WHERE
+    key=$2;
+
+-- name: update-meter-location
+UPDATE geodata.smartmeters
+SET
+    geometry=ST_Point ($1, $2, 4326)
+WHERE
+    key=$3;
+
+-- name: update-meter-properties
+UPDATE geodata.smartmeters
+SET
+    additional_properties=$1
+WHERE
+    key=$2;
